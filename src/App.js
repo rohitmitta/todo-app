80892e00eb75ca57aa7f20adcf73d08bfbe10c5d
Rohit Mitta <rohitmitta.it@gmail.com>
2019-05-25 23:24:47 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,58 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ToDoApp extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { items: [], text: '' };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>ToDo List</h3>
+        <ToDoList items={this.state.items} />
+        <label htmlFor='new-todo'>What needs to be done?</label>
+        <input id='new-todo' onChange={this.handleChange} value={this.state.text} />
+        <form onSubmit={this.handleSubmit}>
+          <button>Add #{this.state.items.length + 1}</button>
+        </form>
+      </div>
+    );
+  }
+
+  handleChange(e) {
+    this.setState({ text: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (!this.state.text) {
+      return
+    }
+    const newItem = {
+      text: this.state.text,
+      id: Date.now()
+    };
+    this.setState(state => ({
+      items: state.items.concat(newItem),
+      text: ''
+    }));
+
+  }
 }
 
-export default App;
+class ToDoList extends React.Component {
+  render() {
+    return (
+      this.props.items.map(item => (
+        <li key={item.id}>{item.text}</li>
+      ))
+    )
+  }
+}
+
+export default ToDoApp;
