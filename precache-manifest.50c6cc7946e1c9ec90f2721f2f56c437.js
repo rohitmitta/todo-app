082153a7377fd528641237c198d0c418bd57be19
Rohit Mitta <rohitmitta.it@gmail.com>
2019-05-25 23:45:59 +0530

    Updates

---
+++ precache-manifest.50c6cc7946e1c9ec90f2721f2f56c437.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "666ee960c37ad5a0aae0f90bbc5a00bd",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "8b635b7334ca8c0fc3b5",
+    "url": "/todo-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/todo-app/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "8b635b7334ca8c0fc3b5",
+    "url": "/todo-app/static/js/main.ce0241f7.chunk.js"
+  },
+  {
+    "revision": "f7728c30f6339eef41ed",
+    "url": "/todo-app/static/js/runtime~main.eca9ff85.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
